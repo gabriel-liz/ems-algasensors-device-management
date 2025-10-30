d093410902ebebcf0d19ebe87ce7100e5be44fb8
gabriel-liz <gabriel.liz@outlook.com.br>
2025-10-30 13:32:13 -0300

    9.5. Usando o TSID com Java Criadas classes utilitarias para gerar o TSID e extrair a Data do TSID e também criados Testes unitarios para essa geração/extração

---
+++ src/main/java/com/algaworks/algasensors/device/management/IdGenerator.java
@@ -0,0 +1,31 @@
+package com.algaworks.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+
+import java.util.Optional;
+
+public class IdGenerator {
+
+    private static final TSID.Factory tsidFactory;
+
+    static {
+        Optional.ofNullable(System.getenv("tsid.node"))
+                        .ifPresent(tsidNode -> System.getProperty("tsid.node", tsidNode));
+
+        Optional.ofNullable(System.getenv("tsid.node"))
+                .ifPresent(tsidNodeCount -> System.getProperty("tsid.node.count", tsidNodeCount));
+
+
+//        System.setProperty("tsid.node", "2");
+//        System.setProperty("tsid.node.count", "32");
+        tsidFactory = TSID.Factory.builder().build();
+    }
+
+    private IdGenerator() {
+
+    }
+
+    public static TSID generateTSID(){
+        return tsidFactory.generate();
+    }
+}
---
+++ src/test/java/com/algaworks/algasensors/device/management/TSIDTest.java
@@ -0,0 +1,59 @@
+package com.algaworks.algasensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class TSIDTest {
+
+    @Test
+    void shouldGenerateTSID(){
+
+        TSID tsid = IdGenerator.generateTSID();
+
+//        System.out.println(tsid);
+//        System.out.println(tsid.toLong());
+//        System.out.println(tsid.getInstant());
+
+        Assertions.assertThat(tsid.getInstant())
+                .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println(tsid);
+        System.out.println(tsid.toLong());
+        System.out.println(tsid.getInstant());
+    }
+}
