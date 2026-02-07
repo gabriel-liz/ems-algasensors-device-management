959e2675bd52c570b1508bd558eba5f27fff2a71
gabriel-liz <gabriel.liz@outlook.com.br>
2026-02-07 11:56:17 -0300

    11.4. Lidando com erros de timeout * Em SensorMonitoringClientImpl.generateClientHttpRequestFactory foi setado o tempo que é aguardado até gerar o timeOut * Em SensorMonitoringController foi forçado um thread.sleep para gerar o timeOut * Criada classe ApiExceptionHandler para retornar os status code de acordo com as exception e tratado o body de retorno

--- src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -4,8 +4,12 @@ import com.algaworks.algasensors.device.management.api.client.SensorMonitoringCl
 import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -15,10 +19,20 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
     public SensorMonitoringClientImpl(RestClient.Builder builder) {
         //Com isso qualquer erro que o RestClient venha a tratar, erros do tipo 400..., 500, vai cair nesse método e a excessão será transformada na excessão SensorMonitoringClientBadGatewayException
         this.restClient = builder.baseUrl("http://localhost:8082")
+                .requestFactory(generateClientHttpRequestFactory())
                 .defaultStatusHandler(HttpStatusCode::isError, ((request, response) -> {
                     throw new SensorMonitoringClientBadGatewayException();
                 }))
                 .build();
+    }
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(Duration.ofSeconds(5));
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+
+
+        return factory;
     }
 
     //aqui faz a chamada de um microserviço pro outro
